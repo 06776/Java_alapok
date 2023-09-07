12d8f471796bdef79d74ab95c1e0e5bbc2908fe5
06776 <hajdarapatrik11@outlook.com>
2023-09-07 12:16:43 +0200

    Filekez.java modositva

--- src/Filekez.java
+++ src/Filekez.java
@@ -3,7 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Filekez {
+
     public void beolvas() throws FileNotFoundException {
+        try {
+            tryBeolvas();
+        } catch (FileNotFoundException e) {
+            System.err.println("Hiba! Nem található a fájl!");
+        }
+    }
+    public void tryBeolvas() throws FileNotFoundException {
         File file = new File("c:/Users/hajda/Documents/Developer/Alapok - Java/second/Valami.txt");
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
